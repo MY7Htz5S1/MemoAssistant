6691cf1fb44af3333919c9f1cd64d396dc86b75c
LiuMuzhe <lmz1254601702lmz@163.com>
2025-05-22 21:38:39 +0800

    Signed-off-by: LiuMuzhe <lmz1254601702lmz@163.com>

---
+++ Pages/p_manage.cpp
@@ -0,0 +1,4 @@
+#include "p_manage.h"
+
+P_Manage::P_Manage(QWidget* parent):
+    ElaScrollPage(parent) {}
---
+++ Pages/p_manage.h
@@ -0,0 +1,12 @@
+#ifndef P_MANAGE_H
+#define P_MANAGE_H
+
+#include "ElaScrollPage.h"
+class P_Manage : public ElaScrollPage
+{
+    Q_OBJECT
+public:
+    P_Manage(QWidget* parent = nullptr);
+};
+
+#endif // P_MANAGE_H
