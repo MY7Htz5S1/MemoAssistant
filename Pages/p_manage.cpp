68d63db7a7694543f89c1b0ced33873d398a24df
LiuMuzhe <lmz1254601702lmz@163.com>
2025-06-01 19:48:16 +0800

    功能更新0601
    -修改pManage的界面，增加添加事项的按钮

--- Pages/p_manage.cpp
+++ Pages/p_manage.cpp
@@ -1,8 +1,10 @@
 #include "p_manage.h"
 
 #include <ElaMessageBar.h>
+#include <filesystem>
 
 #include "../CustomWidgets/TaskCard.h"
+#include "../TaskManageEventBus.h"
 
 P_Manage::P_Manage(TaskList& tskList, QWidget* parent):
     ElaScrollPage(parent){
@@ -10,23 +12,80 @@ P_Manage::P_Manage(TaskList& tskList, QWidget* parent):
     widget = new QWidget(this);
     setWindowTitle("事项管理");
     setTitleVisible(false);
-    layout = new ElaFlowLayout(widget);
-    layout->setIsAnimation(true);
+
+    layout = new QVBoxLayout(widget);
+    layout->setSpacing(10);
+    title = new ElaText("事项管理",35,this);
+    title->setFixedHeight(35);
+    layout->addWidget(title);
+
+    layout->addSpacing(5);
+
+    textAndButtonLayout = new QHBoxLayout();
+    description = new ElaText("下方是你所有的事项",15,this);
+    description->setFixedHeight(25);
+    textAndButtonLayout->addWidget(description);
+    textAndButtonLayout->addStretch();
+    
+    addButton = new ElaIconButton(ElaIconType::CirclePlus,20,20,20,this);
+    connect(addButton,&ElaIconButton::clicked,this,&P_Manage::addNewTask);
+    textAndButtonLayout->addWidget(addButton);
+    addButtonToolTip = new ElaToolTip(addButton);
+    addButtonToolTip->setToolTip("添加新事项");
+    layout->addLayout(textAndButtonLayout);
+    layout->addSpacing(5);
+
+
+    cardGroup = new QGroupBox(widget);
+    cardLayout = new ElaFlowLayout(cardGroup);
+    cardLayout->setIsAnimation(true);
     updateCards(tskList);
+
+    layout->addWidget(cardGroup);
+    layout->addStretch();
     setCustomWidget(widget);
     addCentralWidget(widget);
 }
 void P_Manage::updateCards(TaskList& tasks) {
-    while(layout->count()) {
-        const auto item = layout->itemAt(0);
-        layout->removeItem(item);
+    while(cardLayout->count()) {
+        const auto item = cardLayout->itemAt(0);
+        cardLayout->removeItem(item);
         delete item->widget();
     }
     update();
     for(auto &p:tasks) {
         auto *task = new TaskCard(p,widget);
-        layout->addWidget(task);
+        cardLayout->addWidget(task);
         qDebug()<<"addCard:"<<p.taskName;
     }
     update();
+}
+
+void P_Manage::addNewTask() {
+    auto manageTaskWidget = new ManageTaskWidget(); // 创建新的Widget实例
+    manageTaskWidget->setWindowModality(Qt::ApplicationModal);
+    manageTaskWidget->setWindowFlag(Qt::WindowStaysOnTopHint);
+    connect(manageTaskWidget, &ManageTaskWidget::taskSaved, this, [=] (const Task& t){
+        manageTaskWidget->close();
+        bool ok;
+        emit tManage->TaskAdded(t, ok);
+        if (ok) {
+            // 更新任务列表和Docker
+            //mainWindow->initDB(mainWindow->getCurrentDatabaseName());
+            ElaMessageBar::success(
+                ElaMessageBarType::TopRight,
+                "保存成功",
+                "备忘录已成功创建",
+                2000
+                );
+        } else {
+            ElaMessageBar::error(
+                ElaMessageBarType::TopRight,
+                "保存失败",
+                "无法保存备忘录，请重试",
+                2000
+                );
+        }
+    });
+    manageTaskWidget->show();
 }
--- Pages/p_manage.h
+++ Pages/p_manage.h
@@ -2,20 +2,34 @@
 #define P_MANAGE_H
 
 #include <ElaFlowLayout.h>
+#include <ElaPushButton.h>
 #include <ElaWidget.h>
+#include <ElaIconButton.h>
+#include <ElaToolTip.h>
+#include <QGroupBox>
 
 #include "ElaScrollPage.h"
 #include "../publicheader.h"
 class P_Manage : public ElaScrollPage
 {
     Q_OBJECT
+    ElaText *title;
+    ElaText *description;
+
+    QVBoxLayout *layout{nullptr};
+    QHBoxLayout *textAndButtonLayout{nullptr};
+    ElaIconButton *addButton{nullptr};
+    ElaToolTip *addButtonToolTip{nullptr};
+
     typedef QVector<Task> TaskList;
     QWidget* widget{};
-    ElaFlowLayout* layout{nullptr};
+    ElaFlowLayout* cardLayout{nullptr};
+    QGroupBox *cardGroup{nullptr};
 
 public:
     explicit P_Manage(TaskList& tskList, QWidget* parent = nullptr);
     void updateCards(TaskList& tasks);
+    void addNewTask();
 };
 
 #endif // P_MANAGE_H
